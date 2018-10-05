4fa165a5d81b46d74b78da2171e6c545af43d5b9
Xavi Torelló <info@xaviertorello.cat>
2018-10-05 19:51:58 +0200

    Assign the path from the settings

--- polymorph/UI/interface.py
+++ polymorph/UI/interface.py
@@ -21,16 +21,7 @@ class Interface(object):
 
     def __init__(self):
         self._poisoner = None
-        self._polym_path = os.path.expanduser("~/.polymorph")
-
-        # TODO; Just a PoC with hardcoded dirs, integrate some kind of settings
-        # TODO; Ensure that all App reuse this iface._polym_path instead of redefine it
-        # Iterate all paths down the "~/.polymorph" to initialize it if needed
-        _conditions_path = ["", "conditions", "conditions/executions", "conditions/postconditions", "conditions/preconditions"]
-        for _path in _conditions_path:
-            _full_path = "{}/{}".format(self._polym_path, _path)
-            if not os.path.exists(_full_path):
-                os.makedirs(_full_path)
+        self._polym_path = polymorph.settings.path
 
     @staticmethod
     def print_help(hdict):
