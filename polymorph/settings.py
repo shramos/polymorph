53832e50ae6cde5ae55ffa9380389a87054e081a
Xavi Torelló <info@xaviertorello.cat>
2018-10-05 21:32:43 +0200

    Create templates path at settings

--- polymorph/settings.py
+++ polymorph/settings.py
@@ -12,10 +12,10 @@ class Settings (object):
             "preconditions": "{}/conditions/preconditions".format(self.path),
             "postconditions": "{}/conditions/postconditions".format(self.path),
             "executions": "{}/conditions/executions".format(self.path),
+            "templates": "{}/templates".format(self.path),
         }
 
         # TODO; Ensure that all App reuse this iface._polym_path instead of redefine it
         for _path in self.paths.values():
-            _full_path = "{}/{}".format(self.path, _path)
-            if not os.path.exists(_full_path):
-                os.makedirs(_full_path)
+            if not os.path.exists(_path):
+                os.makedirs(_path)
