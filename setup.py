b8da9571a5599f3437260752f53d8c6ddb8d9da2
shramos <shramos@protonmail.com>
2020-10-09 13:31:58 -0400

    UPDATED: v2.0.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ elif platform.system() == "Windows":
 
 setup(
     name="polymorph",
-    version="2.0.4",
+    version="2.0.5",
     packages=find_packages('.'),
     scripts=SCRIPTS,
     license="MIT",
